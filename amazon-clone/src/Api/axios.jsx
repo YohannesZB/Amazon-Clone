54802192e202d40ed66afce2a77c3d370350e156
Kedus1922 <152933553+Kedus1922@users.noreply.github.com>
2024-02-19 18:54:31 -0500

    axios

--- amazon-clone/src/Api/axios.jsx
+++ amazon-clone/src/Api/axios.jsx
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-
-  
     baseURL: "http://127.0.0.1:5001/clone-408b8/us-central1/api",
     baseURL: "https://api-zkq44uq7xa-uc.a.run.app"
    
 });
-
 export {axiosInstance};
